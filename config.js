de385e3541e3d7534252c56e76a4574b691c2b15
airpower01 <mudhah@airpower.co.id>
2023-12-04 19:02:21 +0700

    updating config.js

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
-import dotenv from 'dotenv';
-import assert from 'assert';
+// import dotenv from 'dotenv';
+// import assert from 'assert';
 
-dotenv.config();
+// dotenv.config();
 
-const {
-  PORT,
-  HOST,
-  HOST_URL,
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-} = process.env;
+// const {
+//   PORT,
+//   HOST,
+//   HOST_URL,
+//   API_KEY,
+//   AUTH_DOMAIN,
+//   PROJECT_ID,
+//   STORAGE_BUCKET,
+//   MESSAGING_SENDER_ID,
+//   APP_ID,
+// } = process.env;
 
-assert(PORT, 'Port is required');
-assert(HOST, 'Host is required');
+// assert(PORT, 'Port is required');
+// assert(HOST, 'Host is required');
 
+
+// put directly the parameters here!
 export default {
   port: PORT,
   host: HOST,
